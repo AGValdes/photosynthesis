8bb01a9d259234b701723646f94ea2709087eb77
Michael Falk <falk478@yahoo.com>
2020-11-23 17:00:41 -0800

    added search box and url with working search results

--- server.js
+++ server.js
@@ -8,42 +8,47 @@ const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
 const cors = require('cors');
-const fetch = require('node-fetch');
+
 
 const KEY = process.env.KEY;
 const PORT = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
 
-
+app.use(cors());
 app.use(methodOverride('_method'));
 app.use('/public', express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.get('/hello', helloWorld);
+app.get('/', helloWorld);
 app.get('/search', getPlants);
 
 
 function getPlants(req, res) {
-    let url = `https://trefle.io/api/v1/plants?token=${KEY}`;
-    superagent.get(url)
-        .then(results => {
-            console.log(url);
-            console.log(results.body);
-        })
-        .catch(err => console.error(err));
+  let currentSearch = req.query.namedsearch;
+  console.log(req.query);
+    let url = `https://trefle.io/api/v1/plants/search?token=${KEY}&q=${currentSearch}`;
+  
+  
+  
+  superagent.get(url)
+    .then(results => {
+      console.log(url);
+    console.log(results.body);
+    })
+    .catch(err => console.error(err));
 }
 
 
 function helloWorld(req, res) {
-    res.render('./pages/index');
+  res.render('./pages/index');
 }
 
 app.use('*', (req, res) => {
-    res.status(404).send('Sorry that does not exist.');
+  res.status(404).send('Sorry that does not exist.');
 })
 
 app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
+  console.log(`server is running on ${PORT}`);
 })
